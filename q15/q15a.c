a50e8e8102c9f9caa7f9f8d4be3a9dc5bb78d51b
CoffeeCoder1015 <heroicnathan@gmail.com>
2024-12-27 22:55:57 +1300

    q15a testing correct

--- q15/q15a.c
+++ q15/q15a.c
@@ -1,4 +1,6 @@
 #include <corecrt.h>
+#include <math.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -39,8 +41,14 @@ int main() {
 
     x_size = strchr(map, '\n')-map;
 
-    printf("%s",map);
-    printf("%d %d %d,%d",x_size,y_size,pos[0],pos[1]);
+    size_t current_position = ftell(inputs);
+    fseek(inputs, 0, SEEK_END);
+    size_t end_position = ftell(inputs);
+    size_t read_count = end_position-current_position;
+    fseek(inputs, current_position, SEEK_SET);
+    char* instructions = malloc(sizeof(char)*(read_count+1));
+    fread(instructions,sizeof(char),read_count,inputs);
+    instructions[read_count-1] = '\0';
     // direction chr codes
     // <  60
     // v 118
@@ -55,38 +63,63 @@ int main() {
     // if wall => continue to next direction command
     // if space => repalce space with last thing read in before it. Replace robot with space. And if box in front of robot's old position repalce it with the robot
     int directions[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
-    for (;;) {
-        char buffer[1024];
-        char* r = fgets(buffer, 1024, inputs);
-        if (r == NULL) {
-            break; 
+    for (int i = 0; i < read_count; i++) {
+        char command = instructions[i];
+        if (command == '\n') {
+            continue;
         }
-        for (int i = 0; buffer[i] != '\n'; i++) {
-            char command = buffer[i];
-            int direction_index = 0;
-            switch (command) {
-                case '<': 
-                    direction_index = 0;
-                    break;
-                case 'v':
-                    direction_index = 1;
-                    break;
-                case '>':
-                    direction_index = 2;
-                    break;
-                case '^':
-                    direction_index = 3;
-                    break;
-            } 
-            int* direction_vec = directions[direction_index];
-            for (int mult = 0;;mult++) {
-                int next_pos[2]  = {pos[0]+direction_vec[0],pos[1]+direction_vec[1]};
-                int next_index = next_pos[1]*(x_size+1)+next_pos[0];
+        int direction_index = 0;
+        switch (command) {
+            case '<': 
+                direction_index = 0;
+                break;
+            case 'v':
+                direction_index = 1;
+                break;
+            case '>':
+                direction_index = 2;
+                break;
+            case '^':
+                direction_index = 3;
+                break;
+        } 
+        int* direction_vec = directions[direction_index];
+        int termination_index = 0;
+        int current_index = pos[1]*(x_size+1)+pos[0];
+        bool isWall = false;
+        bool last_is_box = false;
+        for (int mult = 1;;mult++) {
+            int next_pos[2]  = {pos[0]+direction_vec[0]*mult,pos[1]+direction_vec[1]*mult};
+            int next_index = next_pos[1]*(x_size+1)+next_pos[0];
+            bool wall_found = map[next_index] == '#';
+            if (wall_found || map[next_index] == '.') {
+                termination_index = next_index;
+                int previous_pos[2] = {next_pos[0]-direction_vec[0],next_pos[1]-direction_vec[1]};
+                int previous_index = previous_pos[1]*(x_size+1)+previous_pos[0];
+                last_is_box = map[previous_index] == 'O';
+                isWall = wall_found;
+                break;
             }
         }
+        if (!isWall) {
+            map[current_index] = '.';
+            pos[0] += direction_vec[0];
+            pos[1] += direction_vec[1];
+            current_index = pos[1]*(x_size+1)+pos[0];
+            map[current_index] = '@';
+            if (last_is_box) {
+                map[termination_index] = 'O';
+            } 
+        }
     }
-
-
+    // printf("%s",map);
+    int cord_sum = 0;
+    for (int i = 0; i < ptr_pos; i++) {
+        if(map[i] == 'O'){
+            cord_sum += i/(x_size+1)*(99-x_size)+i;
+        }
+    }
+    printf("%d\n",cord_sum);
     free(map);
     fclose(inputs);
 }
