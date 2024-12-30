ff285aa9e41d2dd686550246a9cceac513c5b523
CoffeeCoder1015 <heroicnathan@gmail.com>
2024-12-30 23:35:40 +1300

    New hashmap
    - list based key storage to avoid collisions
    
    - void* value type so any value can be stored
    
    - Using self-defined free functions so if value type is heap allocated
      it can be freed easily too
    
    - key value stays as uint64_t but conversion function is used to make
      types conform to the necessary key type for hashing

--- q16/q16a.c
+++ q16/q16a.c
@@ -1,6 +1,7 @@
 #include <corecrt.h>
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -128,6 +129,156 @@ mheap_item minheap_extract(minheap* mh){
     return item;
 }
 
+
+typedef struct{
+    uint64_t key;
+    void* value;
+} map_entry;
+
+typedef struct{
+    size_t length;
+    map_entry* kv_paris;
+} map_slot;
+
+typedef struct{
+    map_slot *entries;
+    size_t capacity;    // size of _entries array
+    size_t length;  // number of items in hash table
+    //used to free the indivual values in kv pairs
+    //set to NULL if void* value does not actually point to a heap allocated resource
+    void (*free_entry)(map_entry*);
+} hashmap;
+
+#define INIT_CAPACITY 6400
+hashmap* new_hashmap(void (*free_function)(map_entry*)){
+    hashmap* h = malloc(sizeof(hashmap));
+    h->length = 0;
+    h->capacity = INIT_CAPACITY;
+    h->free_entry = free_function;
+
+    h->entries = calloc(h->capacity, sizeof(map_slot));
+    return h;
+}
+
+void free_hashmap(hashmap* h){
+    for (size_t i = 0; i < h->capacity; i++) {
+        if (h->entries[i].length > 0) {
+            if (h->free_entry != NULL) {
+                for (size_t j = 0; j < h->entries[i].length; j++) {
+                    h->free_entry(&h->entries[i].kv_paris[j]);
+                }
+            }
+            free(h->entries[i].kv_paris);
+        }
+    }
+    free(h->entries);
+    free(h);
+}
+
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+static uint64_t hash_key(uint64_t key) {
+    uint64_t hash = FNV_OFFSET;
+    for (int byte_offset= 0; byte_offset < 8; byte_offset++) {
+        uint8_t byte_of_data = ( key >> (byte_offset * 8) ) & 255;
+        hash *= FNV_PRIME;
+        hash ^= byte_of_data;
+    }
+    return hash;
+}
+
+uint64_t pos_to_key(int32_t pos[2]){
+    uint64_t key = (uint64_t)pos[0] << 32;
+    key |= pos[1];
+    return key;
+}
+
+uint64_t* map_set_entry(map_slot* entries, size_t capacity, size_t* plength, uint64_t key, void* value) {
+    uint64_t hash = hash_key(key);
+    size_t index = (size_t)(hash & (uint64_t)(capacity - 1));
+
+    for (size_t i = 0; i < entries[index].length; i++) {
+        map_entry* current_entry = &entries[index].kv_paris[i];
+        if (current_entry->key == key) {
+            //update value
+            current_entry->value = value;
+            return &current_entry->key;
+        }
+    }
+    //if key wasnt found
+    if (plength != NULL) {
+        ( *plength )++; 
+    }
+    map_entry entry = {key,value};
+    entries[index].length++;
+    if (entries[index].length == 1) {
+        entries[index].kv_paris = malloc(sizeof(map_entry));
+    }else {
+        entries[index].kv_paris = realloc(entries[index].kv_paris, sizeof(map_entry)*entries[index].length);
+    }
+    map_entry* last = &entries[index].kv_paris[entries[index].length-1];
+    *last = entry;
+    return &last->key;
+}
+
+bool map_expand(hashmap* h){
+    size_t new_capacity = h->capacity * 2;
+    if (new_capacity < h->capacity) {
+        return false;  // overflow (capacity would be too big)
+    }
+    map_slot* new_entries = calloc(new_capacity, sizeof(map_slot));
+    if (new_entries == NULL) {
+        return false;
+    }
+
+   // Iterate entries, move all non-empty ones to new h's entries.
+    for (size_t i = 0; i < h->capacity; i++) {
+        map_slot entry = h->entries[i];
+        for (size_t j = 0; j < entry.length; j++) {
+            map_set_entry(new_entries, new_capacity, NULL, entry.kv_paris[j].key,entry.kv_paris[j].value);
+        }
+    }
+    
+    free(h->entries);
+    h->capacity = new_capacity;
+    h->entries = new_entries;
+    return true;
+}
+
+uint64_t* map_set(hashmap* h, int32_t pos[2], void* value){
+    if (h->length >= h->capacity / 2) {
+        if (!map_expand(h)) {
+            return NULL;
+        }
+    }
+    uint64_t key = pos_to_key(pos);
+    uint64_t* result = map_set_entry(h->entries,h->capacity,&h->length,key,value);
+    return result;
+
+}
+
+typedef struct{
+    bool found;
+    void* value;
+} result;
+
+result map_get(hashmap* h, int32_t pos[2]){
+    uint64_t key = pos_to_key(pos);
+    uint64_t hash = hash_key(key);
+    size_t index = (size_t)(hash & (uint64_t)(h->capacity - 1));
+    map_slot hashed_slot = h->entries[index];
+    if (hashed_slot.length == 0) {
+        return (result){false,NULL}; 
+    }
+
+    for (size_t i = 0; i < hashed_slot.length; i++) {
+        map_entry current_entry = hashed_slot.kv_paris[i];
+        if (current_entry.key == key) {
+            return (result){true,current_entry.value}; 
+        }
+    }
+    return (result){false,NULL}; 
+}
 int main(){
     FILE* inputs;
     errno_t err = fopen_s(&inputs , "test.txt", "rb");
