ee996872a9eff58eb085bb0ed01d5abfb364088c
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-04-23 01:18:56 +1000

    calculates moves for 1st robot

--- q21/q21a.c
+++ q21/q21a.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,9 +29,9 @@ int numpad_coords[11][2] = {{1,0},{2,1},{1,1},{0,1},{2,2},{1,2},{0,2},{2,3},{1,3
 // 1| < v >
 //  -------
 //X= [2 1 0]
-//                           >     v     <     ^     A
-int dirpad_coords[5][2] = {{0,1},{1,1},{2,1},{1,0},{0,0}};
-char dir_glyph[] = ">v<^";
+//                           <     ^     >     v     A
+int dirpad_coords[5][2] = {{2,1},{1,1},{0,1},{1,1},{0,0}};
+char dir_glyph[] = "<^>vA";
 int directions[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
 
 int main(){
@@ -43,8 +44,59 @@ int main(){
         if (end == NULL) {
             break; 
         }
+        int n = strlen(buffer);
+        buffer[--n] = '\0';
 
-        printf("%s",buffer);
+        printf("%s\n",buffer);
+        int base_capacity = 10;
+        int base_size = 0;
+        int* base_array = malloc(base_capacity*sizeof(int));
+
+        int previous_mapping = 10;  // mapping for 'A'
+        for (int i = 0; i < n; i++) {
+            int current_mapping = buffer[i] - '0';
+            if (buffer[i] == 'A') {
+                current_mapping = 10; 
+            }
+
+            int* current = numpad_coords[current_mapping];
+            int* previous = numpad_coords[previous_mapping];
+            int vec_diff[2] = { current[0]-previous[0], current[1]-previous[1] };
+            // printf("M:%d %d,%d -> %d,%d\n",current_mapping,previous[0],previous[1],current[0],current[1]);
+            // printf("%c %d,%d\n",buffer[i],vec_diff[0],vec_diff[1]);
+
+            int x_mag = abs(vec_diff[0]);
+            int y_mag = abs(vec_diff[1]);
+
+            int memory_defecit = base_size + x_mag + y_mag + 2  - base_capacity;
+            if (memory_defecit > 0) {
+                base_capacity += abs(memory_defecit);
+                base_array  = realloc(base_array, sizeof(int)*base_capacity);
+            }
+
+            int xneg = vec_diff[0]>>31 | !!vec_diff[0];
+            for (int j = 0; j < x_mag; j++) {
+                base_size++; 
+                base_array[base_size-1] = 1-xneg;
+            }
+
+            int yneg = vec_diff[1]>>31 | !!vec_diff[1];
+            for (int j = 0; j < y_mag; j++) {
+                base_size++; 
+                base_array[base_size-1] = 2-yneg;
+            }
+
+            base_size++;
+            base_array[base_size-1] = 4;
+            previous_mapping = current_mapping;
+        }
+
+        for (int i = 0; i < base_size; i++) {
+            printf("%c",dir_glyph[base_array[i]]) ;
+        }
+        printf("\n");
+
+        free(base_array);
         char* termination = strchr(buffer,'A');
         *termination = '\0';
         int interger_component = atoi(buffer);
