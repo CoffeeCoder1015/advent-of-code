063559db91f5b8a95c66d83a148a1b91bb5381ae
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-04-23 02:54:48 +1000

    calculates moves for 2nd robot

--- q21/q21a.c
+++ q21/q21a.c
@@ -8,18 +8,18 @@
 // 'A' mapped to index 10, so that the rest of the numbers are a perfect hash
 //
 //Y=
-// 3|7 8 9
-// 2|4 5 6
-// 1|1 2 3
-// 0|  0 A
+// 0|7 8 9
+// 1|4 5 6
+// 2|1 2 3
+// 3|  0 A
 //  ------
-//X=[2 1 0]
+//X=[0 1 2]
 //
 //
 //
 //
 //                            0     1     2     3     4     5     6     7     8     9     A
-int numpad_coords[11][2] = {{1,0},{2,1},{1,1},{0,1},{2,2},{1,2},{0,2},{2,3},{1,3},{0,3},{0,0}};
+int numpad_coords[11][2] = {{1,3},{0,2},{1,2},{2,2},{0,1},{1,1},{2,1},{0,0},{1,0},{2,0},{2,3}};
 
 //Direction pad:
 // 'A' mapped to the end so the directons coords are a perfect hash to the previous questions' dir vector list
@@ -28,11 +28,59 @@ int numpad_coords[11][2] = {{1,0},{2,1},{1,1},{0,1},{2,2},{1,2},{0,2},{2,3},{1,3
 // 0|   ^ A
 // 1| < v >
 //  -------
-//X= [2 1 0]
-//                           <     ^     >     v     A
-int dirpad_coords[5][2] = {{2,1},{1,1},{0,1},{1,1},{0,0}};
-char dir_glyph[] = "<^>vA";
+//X= [0 1 2]
+//                           >     v     <     ^     A
+int dirpad_coords[5][2] = {{2,1},{1,1},{0,1},{1,0},{2,0}};
+char dir_glyph[] = ">v<^A";
 int directions[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+
+typedef struct{
+    int size;
+    int* ptr;
+} array;
+
+array robot_to_robot_instructions(int* instructions, int n){
+    int base_capacity = 10;
+    int base_size = 0;
+    int* base_array = malloc(base_capacity*sizeof(int));
+
+    int previous_mapping = 4;  
+    for (int i = 0; i < n; i++) {
+        int current_mapping = instructions[i];
+
+        int* current = dirpad_coords[current_mapping];
+        int* previous = dirpad_coords[previous_mapping];
+        int vec_diff[2] = { current[0]-previous[0], current[1]-previous[1] };
+        // printf("M:%d %d,%d -> %d,%d\n",current_mapping,previous[0],previous[1],current[0],current[1]);
+        // printf("%c %d,%d\n",buffer[i],vec_diff[0],vec_diff[1]);
+
+        int x_mag = abs(vec_diff[0]);
+        int y_mag = abs(vec_diff[1]);
+
+        int memory_defecit = base_size + x_mag + y_mag + 2  - base_capacity;
+        if (memory_defecit > 0) {
+            base_capacity += abs(memory_defecit);
+            base_array  = realloc(base_array, sizeof(int)*base_capacity);
+        }
+
+        int xneg = vec_diff[0]>>31 | !!vec_diff[0];
+        for (int j = 0; j < x_mag; j++) {
+            base_size++; 
+            base_array[base_size-1] = 1-xneg;
+        }
+
+        int yneg = vec_diff[1]>>31 | !!vec_diff[1];
+        for (int j = 0; j < y_mag; j++) {
+            base_size++; 
+            base_array[base_size-1] = 2-yneg;
+        }
+
+        base_size++;
+        base_array[base_size-1] = 4;
+        previous_mapping = current_mapping;
+    }
+    return (array){base_size,base_array} ;
+}
 
 int main(){
     FILE* input;
@@ -96,7 +144,13 @@ int main(){
         }
         printf("\n");
 
+        array r2 = robot_to_robot_instructions(base_array,base_size);
         free(base_array);
+        int* l2 = r2.ptr;
+        for (int i = 0; i < r2.size; i++) {
+            printf("%c",dir_glyph[l2[i]]) ;
+        }
+        printf("\n");
         char* termination = strchr(buffer,'A');
         *termination = '\0';
         int interger_component = atoi(buffer);
