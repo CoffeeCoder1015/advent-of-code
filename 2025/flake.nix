189ba37da4db16934a08f651f574c5fded97e697
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-12-01 09:51:13 -0800

    nix did some bullshit

---
+++ 2025/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "OCaml dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        ocamlPkgs = pkgs.ocamlPackages;
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            ocamlPkgs.ocaml
+            ocamlPkgs.dune_2
+            ocamlPkgs.findlib
+            ocamlPkgs.merlin
+            ocamlPkgs.ocamlformat
+          ];
+        };
+
+        packages.default = ocamlPkgs.ocaml;
+      });
+}
