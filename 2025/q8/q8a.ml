e0b6a8888f4a506c1c3a86c869454b7449116802
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-12-20 15:58:04 +0800

    replaced linked list traversal with array to improve speed

--- 2025/q8/q8a.ml
+++ 2025/q8/q8a.ml
@@ -17,25 +17,39 @@ let to_coord_array raw =
 let in_chan = open_in "q8.txt" in
   let length = in_channel_length in_chan in
   let raw_coords = String.split_on_char '\n' ( really_input_string in_chan length ) in
-  (* let n = List.length raw_coords in *)
-  let coords = to_coord_array raw_coords in
-  let sum acc x = acc+.x in
-  let dist a b = List.fold_left sum 0. ( List.map2 (fun x y -> ( float_of_int (x-y) )**2.) a b) in
-  let rec calc_dist acc remaining = 
-    if List.is_empty remaining then
-      acc
-    else
-    let focus = List.hd remaining in
-    let rest = List.tl remaining in
-    let dl = List.map (fun x -> (focus, x, dist focus x )) rest in
-    let new_acc = acc @ dl in
-    calc_dist new_acc rest
-  in
-  let dist_pairings = calc_dist [] coords in
-  let pair_compare (_,_,dist1) (_,_,dist2) = int_of_float ( dist1-.dist2 ) in
-  let sorted_pairings = List.sort pair_compare dist_pairings in
+  let n = List.length raw_coords in
+  let coords = Array.of_list ( to_coord_array raw_coords ) in
+  let dist_pairings = Array.make (n*(n-1)/2) ([0;0;0],[0;0;0],0) in
 
-  (* let print_func (a,b,dist) = Printf.printf "%d,%d,%d %d,%d,%d %f\n" (List.nth a 0) (List.nth a 1) (List.nth a 2) (List.nth b 0) (List.nth b 1) (List.nth b 2) dist in
+  let sum acc x = acc+x in
+  let isqr a = a * a in
+  let dist a b = List.fold_left sum 0 ( List.map2 (fun x y -> isqr (x-y)) a b) in
+
+
+  let rec calc_dist i offset = 
+    if i = n then
+      ()
+    else
+    let cur = coords.(i) in
+    let rec helper j = 
+      if j+i+1  = n then
+        ()
+      else
+      let inner = coords.(j+i+1) in
+      let pairing = (cur,inner,dist cur inner) in
+      dist_pairings.(offset+j) <- pairing;
+      helper (j+1) 
+    in
+    helper 0;
+    calc_dist (i+1) (offset+n-i-1)
+  in
+  calc_dist 0 0;
+  
+  let pair_compare (_,_,dist1) (_,_,dist2) = ( dist1-dist2 ) in
+  Array.sort pair_compare dist_pairings;
+  let sorted_pairings = Array.to_list dist_pairings in
+
+  (* let print_func (a,b,dist) = Printf.printf "%d,%d,%d %d,%d,%d %d\n" (List.nth a 0) (List.nth a 1) (List.nth a 2) (List.nth b 0) (List.nth b 1) (List.nth b 2) dist in
   List.iter print_func sorted_pairings; *)
 
   let set_inserter set (a,b,_) = 
