bed75e075ce6d9ebd42d52358ac723e9b68bc7ab
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-12-20 12:07:56 +0800

    promising results from test

--- 2025/q8/q8a.ml
+++ 2025/q8/q8a.ml
@@ -6,6 +6,7 @@ let to_coord_array raw =
 let in_chan = open_in "test.txt" in
   let length = in_channel_length in_chan in
   let raw_coords = String.split_on_char '\n' ( really_input_string in_chan length ) in
+  (* let n = List.length raw_coords in *)
   let coords = to_coord_array raw_coords in
   let sum acc x = acc+.x in
   let dist a b = List.fold_left sum 0. ( List.map2 (fun x y -> ( float_of_int (x-y) )**2.) a b) in
@@ -21,7 +22,61 @@ let in_chan = open_in "test.txt" in
   in
   let dist_pairings = calc_dist [] coords in
   let pair_compare (_,_,dist1) (_,_,dist2) = int_of_float ( dist1-.dist2 ) in
-  let print_func (a,b,dist) = Printf.printf "%d,%d,%d %d,%d,%d %f\n" (List.nth a 0) (List.nth a 1) (List.nth a 2) (List.nth b 0) (List.nth b 1) (List.nth b 2) dist in
   let sorted_pairings = List.sort pair_compare dist_pairings in
-  List.iter print_func sorted_pairings;
 
+  (* let print_func (a,b,dist) = Printf.printf "%d,%d,%d %d,%d,%d %f\n" (List.nth a 0) (List.nth a 1) (List.nth a 2) (List.nth b 0) (List.nth b 1) (List.nth b 2) dist in
+  List.iter print_func sorted_pairings; *)
+
+  let set_inserter set (a,b,_) = 
+    Hashtbl.replace set a ();
+    Hashtbl.replace set b ();
+  in
+  
+  let print_int_list ilist =
+    let n = List.length ilist in
+    List.iteri (fun i x -> 
+      if i != n-1 then
+        Printf.printf "%d," x
+      else
+        Printf.printf "%d" x
+      ) ilist
+    in
+
+  let merge_table tab_a tab_b = 
+    Hashtbl.iter (fun k () -> Hashtbl.replace tab_a k () ) tab_b;
+    tab_a
+  in
+  let rec iter_n i f list circuits =
+    if f = i then
+      circuits
+    else
+      match list with
+      | [] -> circuits 
+      | h::t ->
+      let (junct1,junct2,_) = h in
+      let matched_circuits = List.filter (fun x -> Hashtbl.mem x junct1 || Hashtbl.mem x junct2) circuits in
+      let len_matched = List.length matched_circuits in
+      if len_matched = 0 then
+        let new_set = Hashtbl.create 2 in
+        let new_circuits = circuits @ [new_set] in
+        set_inserter new_set h;
+        iter_n (i+1) f t new_circuits
+      else if len_matched = 1 then 
+        ( set_inserter ( List.hd matched_circuits ) h;
+        iter_n (i+1) f t circuits )
+      else
+        (
+        let unmatched_circuits = List.filter (fun x -> not ( Hashtbl.mem x junct1 || Hashtbl.mem x junct2 )) circuits in
+        let new_merged = List.fold_left merge_table ( Hashtbl.create 0 )  matched_circuits in
+        let new_circuits = unmatched_circuits @ [new_merged] in
+        iter_n (i+1) f t new_circuits )
+    in
+  let circuits = iter_n 0 10 sorted_pairings [] in
+
+  let print_set empty_htbl = 
+    let set = Hashtbl.to_seq_keys empty_htbl in
+    Printf.printf "%d  :  " (Seq.length set) ;
+    set |> Seq.iter (fun x -> print_int_list x; print_char ' ');
+    print_newline ()
+  in
+  List.iter print_set circuits
