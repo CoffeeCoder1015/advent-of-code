e75353480ed72bd666d5d3e080e1a1a0385dc4a7
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-12-23 07:40:44 +0800

    q9b - reverse sorted areas for later checking

--- 2025/q9/q9b.ml
+++ 2025/q9/q9b.ml
@@ -5,73 +5,25 @@ let in_chan = open_in "test.txt" in
   let coords = String.split_on_char '\n' file |> List.map (fun x -> String.split_on_char ',' x |> str_lst_to_xy) |> Array.of_list in
   let n = Array.length coords in
   let coord_area (a,b) (c,d) = ( abs ( a-c )  + 1 ) * (abs ( b-d ) + 1) in
-  let get_other_diag (a,b) (c,d) = ((a,d),(c,b)) in
-  let check_in (a,b) =
-    let rec find_x i acc = 
-      if i = n then
-        acc
-      else
-      let next = (i+1) mod n in
-      let ( cx,cur_y ) = coords.(i) in
-      let ( nx,nxt_y ) = coords.(next) in
-      let lower = min cur_y nxt_y in
-      let upper = max cur_y nxt_y in
-      if lower <= b && b <= upper then
-       find_x (i+1) ( acc @ [cx] @ [nx] )
-      else
-       find_x (i+1) acc
-    in
-    let res = find_x 0 [] in
-    let sres = List.sort compare res in
-    let rec check_x lst = 
-      let arrayed = Array.of_list lst in
-      let arr_len = Array.length arrayed in
-      let rec helper i =
-        if i = arr_len-1 then 
-          false
-        else
-          let current = arrayed.(i) in 
-          let next = arrayed.(i+1) in
-          if current <= a && a <= next then
-            true 
-          else
-            helper (i+1)
-          in
-          helper 0 
-        in
-    let result = check_x sres in
-    (* List.iter (Printf.printf "%d ") sres;
-    print_newline ();
-    Printf.printf "%B\n" result; *)
-    result
-  in 
-  let rec par i best = 
+  let rec par i acc = 
     if i = n then
-      best
+        acc
     else
     let current = coords.(i) in
-    let rec chi j best_c = 
+    let rec chi j acc2 = 
       let idx = j+i+1 in
       if idx = n then
-        best_c
+          acc2
       else
       let focus = coords.(idx) in 
       let area = coord_area focus current in 
-      let (other_a,other_b) = get_other_diag focus current in
-      let all_in = ( check_in other_a  ) &&  ( check_in other_b ) in
-      ( fun (a,b) (c,d) (e,f) (g,h) -> Printf.printf "(%d,%d) (%d,%d) (%d,%d) (%d,%d)" a b c d e f g h ) current focus other_a other_b;
-      Printf.printf " %d %B\n" area all_in;
-      if ( area > best_c ) && all_in then
-        chi (j+1) area
-      else
-        chi (j+1) best_c
+      chi (j+1) ( (current, focus, area) :: acc2 )
       in
-    let best_sub_area = chi 0 best in
-    if best_sub_area > best then
-      par (i+1) best_sub_area
-    else
-      par (i+1) best
+      let acc2_result = chi 0 [] in
+      let new_acc =Array.append (acc2_result |> Array.of_list) acc in
+      par (i+1) new_acc
     in
-    let ans = par 0 0 in
-    Printf.printf "%d\n" ans;
+    let acc = par 0 [||] in
+    Array.sort (fun (_,_,a) (_,_,b) -> b-a) acc;
+    Array.iter (fun ((a,b),(c,d),e) -> Printf.printf "(%d,%d) (%d,%d) %d\n" a b c d e ) acc;
     
