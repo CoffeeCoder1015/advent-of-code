54a129beb21ba201ca60244468e6f73d05011d05
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-12-25 13:34:03 +0800

    compression arrays built [1/2] Coord compression

--- 2025/q9/q9b.ml
+++ 2025/q9/q9b.ml
@@ -1,4 +1,28 @@
-let in_chan = open_in "test.txt" in
+module IntSet = Set.Make(Int)
+
+let get_compression_map coord_arr =
+  let n = Array.length coord_arr in
+  let rec add_to_set i xs ys =
+    if i = n then
+      (xs,ys)
+    else
+      let (x,y) = coord_arr.(i) in
+      let new_xs = IntSet.add x xs in
+      let new_ys = IntSet.add y ys in
+      add_to_set (i+1) new_xs new_ys
+    in
+  let xs,ys = add_to_set 0 IntSet.empty IntSet.empty in
+  let intset_to_array set = set |> IntSet.to_list |> Array.of_list in
+  let xa, ya = intset_to_array xs, intset_to_array ys in
+  Array.sort compare xa; Array.sort compare ya;
+  
+  (* print arrays *)
+  let print_array arr = Array.iter (Printf.printf "%d ") arr; print_newline(); in
+  print_array xa; print_array ya;
+;;
+
+
+let in_chan = open_in "q9.txt" in
   let length = in_channel_length in_chan in
   let file = really_input_string in_chan length in
   let str_lst_to_xy str_lst = (List.nth str_lst 0 |> int_of_string, List.nth str_lst 1 |> int_of_string) in
@@ -26,4 +50,5 @@ let in_chan = open_in "test.txt" in
     let acc = par 0 [||] in
     Array.sort (fun (_,_,a) (_,_,b) -> b-a) acc;
     Array.iter (fun ((a,b),(c,d),e) -> Printf.printf "(%d,%d) (%d,%d) %d\n" a b c d e ) acc;
+    get_compression_map coords
     
