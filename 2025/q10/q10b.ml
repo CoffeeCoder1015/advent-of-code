12e8cb380b7129daf27c1b556eacb396d899d0aa
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-01-05 09:35:32 -0800

    Advent of doom and despair on q10b

--- 2025/q10/q10b.ml
+++ 2025/q10/q10b.ml
@@ -1,3 +1,138 @@
+let to_list = Array.to_list ;;
+
+let vec_0 n = 
+  Array.make n 0
+;;
+
+let add_to_new_hashtbl tbl new_item = 
+  let tmp = Hashtbl.copy tbl in
+  Hashtbl.add tmp new_item ();
+  tmp
+;;
+
+let jolt_plus_btn jolt button = 
+  let tmp = Array.copy jolt in
+  Array.iter (fun idx -> tmp.(idx) <- tmp.(idx) + 1) button;
+  tmp
+;;
+
+let mod2ify arr = 
+  Array.map (fun x -> x mod 2) arr
+;;
+
+let arr_sub main sub = 
+  let tmp = Array.copy main in
+  Array.iteri (fun i x -> tmp.(i) <- tmp.(i) - x) sub;
+  tmp
+;;
+
+let is_all_0 array = Array.fold_left (fun acc x -> if x = 0 then acc else false ) true array ;;
+
+let array_sme array_a array_b = Array.for_all2 (fun a b -> a <= b) array_a array_b;;
+
+let solve joltage available_buttons = 
+  let joltage_n = Array.length joltage in
+  (* let button_n = Array.length available_buttons in *)
+   let mod2_span = 
+
+    let results = Hashtbl.create 0 in
+
+    let insert_result acc n = 
+      let k = mod2ify acc in
+      if not ( Hashtbl.mem results k ) then
+        Hashtbl.add results k (Hashtbl.create 0);
+      let r_tbl = Hashtbl.find results k in
+      match Hashtbl.find_opt r_tbl acc with
+        | Some existing_n when existing_n <= n -> ()
+        | _ -> Hashtbl.replace r_tbl acc n
+    in
+
+    let skip = Hashtbl.create 0 in
+
+    let rec aux vsig acc = 
+
+      Array.iter (fun button -> 
+
+        if not (Hashtbl.mem vsig button) then
+
+          let new_vsig = add_to_new_hashtbl vsig button in
+
+          let new_acc = jolt_plus_btn acc button in
+
+          let reach_req = Hashtbl.length new_vsig in 
+
+          if not (Hashtbl.mem skip new_acc) || (Hashtbl.find skip new_acc > reach_req) then (
+
+            Hashtbl.replace skip new_acc reach_req;
+
+            insert_result new_acc reach_req;
+
+            aux new_vsig new_acc )
+
+        ) available_buttons
+
+    in
+
+    let init_acc = vec_0 joltage_n in
+
+    insert_result init_acc 0;
+
+    aux (Hashtbl.create 0) init_acc;
+
+    results
+
+  in
+
+  (* INFO: Print function helper, no \n*)
+  (* let print_array arr =
+    print_string "[";
+    let n = Array.length arr in
+    Array.iteri (fun i x -> if i+1 < n then Printf.printf "%d," x else Printf.printf "%d" x) arr;
+    print_string "]";
+  in *)
+
+  (* Hashtbl.iter (fun k v ->
+    print_array k ;
+    Seq.iter (fun (acc,n) -> Printf.printf " %d : " n; print_array acc ; print_string " ") (Hashtbl.to_seq v);
+    print_newline()
+    ) mod2_span; *)
+
+  let memo = Hashtbl.create 1024 in 
+  let rec aux remain =
+    if is_all_0 remain then
+      0
+    else
+    match Hashtbl.find_opt memo remain with
+    | Some v -> v
+    | None ->
+      let pattern = mod2ify remain in
+      let next_opts = Hashtbl.find_opt mod2_span pattern in
+      match next_opts with
+      | None -> -1
+      | Some next_opts -> 
+        let best_ret = Seq.fold_left (
+        fun acc (acc_lst,n) -> 
+          let acc_array = acc_lst in
+        let in_bound = array_sme acc_array remain in
+        if in_bound then
+          let subbed = arr_sub remain acc_array in
+          let new_remain = Array.map (fun x -> x/2) subbed in
+          let res = aux new_remain in
+          if res = -1 then acc
+          else
+          let value = n + 2*res in
+          if acc = -1 || value < acc then
+            value
+          else
+            acc
+        else
+          acc
+      ) ( -1 ) ( Hashtbl.to_seq next_opts ) in
+      Hashtbl.add memo remain best_ret;
+      best_ret
+  in
+  aux joltage
+;;
 
 let process_line line = 
   let split = Array.of_list ( String.split_on_char ' ' line ) in
@@ -16,126 +151,10 @@ let process_line line =
     |> List.map int_of_string
     |> Array.of_list 
   in
-
-  let target_len = Array.length jolts_remaning in
-
-  let rel_map = 
-    let n = Array.length buttons in
-    let tmp = Array.make (Array.length jolts_remaning) [||] in
-    for i = 0 to n-1 do
-      let b = buttons.(i) in
-      Array.iter (fun idx -> 
-        let old = tmp.(idx) in
-        let new_arr = Array.append old [|b|] in
-        tmp.(idx) <- new_arr;
-         ) b;
-    done;
-    tmp
-  in
-  
-  let recursive_solve_core j_rem =
-    let odd_comp = Array.map (fun x -> x mod 2) j_rem in
-
-    let _,target_idx = Array.fold_left (
-    fun (i,acc) x -> 
-      if x = 1 then 
-        (i+1,Array.append acc [|i|]) 
-      else 
-        (i+1,acc)
-      ) (0,[||]) odd_comp in
-
-    
-    let sum_by_index raw_arr indicies = 
-      let array = Array.copy raw_arr in
-      Array.iter (fun idx -> array.(idx) <- array.(idx) + 1)  indicies;
-      array
-    in
-    
-    let check_eq array = 
-      let modulation = Array.map (fun x -> x mod 2) array in
-      let _, res = Array.fold_left ( fun (i,eq) x -> if x = odd_comp.(i) then (i+1,eq) else (i+1,false)) (0,true) modulation in
-      res
-    in
-
-    let rec aux queue solns = 
-      let i,sol,r = queue.(0) in
-      if check_eq r then 
-        [|(i,sol,r)|]
-      else
-      let qlen = Array.length queue in
-      let popped_queue = Array.sub queue 1 (qlen-1) in
-      if i >= Array.length target_idx then
-        solns
-      else
-      let idx = target_idx.(i) in
-      let btn_selection = rel_map.(idx) in
-      
-      let new_queue_items, new_solns = Array.fold_left (fun ( acc, solns ) btn -> 
-          if Hashtbl.mem sol btn then
-            ( Array.append acc [|(i+1,sol,r)|],solns )
-          else
-            let new_r = sum_by_index r btn in
-            let new_sol = Hashtbl.copy sol in
-            Hashtbl.add new_sol btn ();
-            if check_eq new_r then
-              (acc, Array.append solns [|(i+1,new_sol,new_r)|])
-            else
-              ( Array.append acc [|(i+1,new_sol,new_r)|], solns )
-      ) ( [||],[||] ) btn_selection in
-      
-      (* if !found then
-        new_queue_items.(0)
-      else *)
-      aux ( Array.append popped_queue new_queue_items ) ( Array.append solns new_solns )
-    in
-
-    let solns = aux [|(0,Hashtbl.create 0,Array.make target_len 0)|] [||] in
-
-    (* Array.iter (
-      fun ( _,s_table,acc ) -> 
-        Printf.printf "-- n:%d --\n" (Hashtbl.length s_table);
-        Seq.iter (fun x -> print_string "[ ";Array.iter (Printf.printf "%d ") x; print_string "]\n";) (Hashtbl.to_seq_keys s_table);
-        print_string "acc: [ ";Array.iter (Printf.printf "%d ") acc; print_string "]\n";
-      ) solns; *)
-
-    Array.map (fun ( _,tbl,acc ) ->
-      let base = Array.copy j_rem in
-      let n = Hashtbl.length tbl in 
-      Array.iteri (fun i x -> base.(i) <- ( base.(i) - x )/2) acc;
-      (n,base)
-    ) solns
-  in
-
-  (* Array.iter (
-    fun ( n,nxt ) -> Printf.printf "%d\n" n; 
-    Array.iter (Printf.printf "%d ") nxt; print_newline();
-  ) solns; *)
-
-  
-  let two_pow exp = int_of_float (2. ** (float_of_int exp)) in
-  let is_all_0 array = Array.fold_left (fun acc x -> if x = 0 then acc else false ) true array in
-  
-  let rec solve_full accum_n target = 
-    if is_all_0 target then 
-      let ans,_ =
-        Array.fold_left (fun (acc,i) x -> ( acc + ( two_pow i ) * x, i+1 ) ) ( 0,0 ) accum_n
-      in
-      ans
-    else
-    let next_targets = recursive_solve_core target in
-    Array.fold_left (fun acc (targ_n, targets ) -> 
-      let inter_ans = solve_full ( Array.append accum_n [|targ_n|] ) targets in
-      if acc = 0 || inter_ans < acc then
-        inter_ans
-      else 
-        acc
-    ) 0 next_targets
-  in
-  let min_ans = solve_full [||] jolts_remaning in
-  min_ans
+  solve jolts_remaning buttons 
 ;;
 
-let in_chan = open_in "test.txt" in 
+let in_chan = open_in "q10.txt" in 
   let rec line_consumer ans = 
     match input_line in_chan with
     | line -> 
