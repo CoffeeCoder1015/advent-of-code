9b503099c0e39a2a4e4f460ea3219c2a2f51692d
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2026-01-05 11:32:34 -0800

    q11a testing works

---
+++ 2025/q11/q11a.ml
@@ -0,0 +1,45 @@
+let in_chan = open_in "test.txt" in
+  let graph = Hashtbl.create 10 in
+  let rec line_consumer _ = 
+    match input_line in_chan with
+    | line -> 
+
+      let n = String.length line in
+      let node = String.sub line 0 3 in
+      let leaves = String.sub line 4 (n-4) |> String.trim |> String.split_on_char ' ' in
+      Hashtbl.add graph node leaves;
+      line_consumer();
+    | exception End_of_file -> 
+        ()
+  in
+  line_consumer();
+  
+  Hashtbl.add graph "out" ["out"];
+  let empty = Hashtbl.to_seq_keys graph |> Seq.map (fun a -> (a,0)) |> Hashtbl.of_seq in
+  
+  let init = 
+    let tmp = Hashtbl.copy empty in
+    Hashtbl.replace tmp "you" 1;
+    tmp
+  in
+
+  let rec solve state = 
+    let new_state = Hashtbl.copy empty in
+    Hashtbl.iter (fun k v ->
+      if v > 0 then
+        let next_nodes = Hashtbl.find graph k in
+        List.iter (fun node -> 
+          let current = Hashtbl.find new_state node in 
+          Hashtbl.replace new_state node (current + v)
+          ) next_nodes;
+      ) state; 
+      
+    let finished = Hashtbl.fold (fun k v acc -> if v = 0 || (v > 0 && k = "out") then acc else false ) new_state true in
+    if finished then
+      new_state
+    else
+      solve new_state
+  in
+  let res = solve init in
+  Printf.printf "%d\n" (Hashtbl.find res "out")
+
